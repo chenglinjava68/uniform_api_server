a5b9961cf38d90c2322bc3c44a354168d48668ba
lixiaopei <xiaopei.li.soar@gmail.com>
2017-01-13 10:01:48 +0800

    shiro config

--- app-web/src/main/java/com/dos/bsapp/model/PrimitiveUser.java
+++ app-web/src/main/java/com/dos/bsapp/model/PrimitiveUser.java
@@ -1,6 +1,6 @@
 package com.dos.bsapp.model;
 
-import java.util.*;
+import java.util.Date;
 
 public class PrimitiveUser
 {
@@ -10,8 +10,41 @@ public class PrimitiveUser
     private String nick;
     private String phone;
     private Date created;
-    public Integer getId() { return id; }
-    public void setId(Integer val){ id = val; }
-    public String getSn() { return sn; }
-    public void setSn(String val){ sn = val; }
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getSn() {
+		return sn;
+	}
+	public void setSn(String sn) {
+		this.sn = sn;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+    
 }
--- app-web/src/main/java/com/dos/bsapp/shiro/ShiroDbRealm.java
+++ app-web/src/main/java/com/dos/bsapp/shiro/ShiroDbRealm.java
@@ -14,13 +14,12 @@ import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
-
-import org.apache.shiro.authc.*;
-import util.MD5;
-
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.dos.bsapp.model.PrimitiveUser;
+
+import util.MD5;
 
 
 public class ShiroDbRealm extends AuthorizingRealm {
@@ -31,38 +30,24 @@ public class ShiroDbRealm extends AuthorizingRealm {
 	 */
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authcToken) throws AuthenticationException {
-		UserTypeNamePasswordToken token = (UserTypeNamePasswordToken) authcToken;
-		String domain = token.getDomain();
+		UsernamePasswordTokenEx token = (UsernamePasswordTokenEx) authcToken;
 		String username = token.getUsername();
         if (username == null) {
         	logger.error("用户名不能为空");
             throw new AccountException("用户名不能为空");
         }
-        if(token.getUserType().equals("user")){
-        	com.intelgoo.qqvisa.po.UserPo user =  accountService.findUserByLoginNameSimple(domain,token.getUsername());
-        	ShiroUser su = new ShiroUser(domain,user.getId(),"user", user.getName(), user.getEmail(), user.getMobile(),
-					user.getRole(), user.getEmEnterpriseId(),user.getBelong());
-        	su.dr = user.getDr();
-
-        	return new SimpleAuthenticationInfo(su,user.getPassword(),getName());
+        
+        try
+        {
+        	PrimitiveUser userInfo = new PrimitiveUser(); // userInfo = userGetService.getBasicBy(token.getUsername(),token.getPassword(),token.getAddition());        
+            ShiroUser su = new ShiroUser(userInfo.getId(),userInfo.getNick(),token.getAddition());      
+            return new SimpleAuthenticationInfo(su,token.getPassword(),username);
         }
-        if(token.getUserType().equals("manager")){
-        	com.intelgoo.qqvisa.model.Manager user =  accountService.findManagerByLoginNameSimple(token.getUsername());
-        	ShiroUser su = new ShiroUser(domain,user.getId(),"manager", user.getName(),"", user.getMobile(),user.getRole(), 0, user.getBelong());
-
-        	return new SimpleAuthenticationInfo(su,user.getPassword(),getName());
+        catch(Exception e){
+        	throw new AccountException("用户名登录失败");
         }
-        if(token.getUserType().equals("sdmanager")){
-        	SubDomain subDomain =  domainService.getByUserName(username);
-        	ShiroUser su = new ShiroUser(subDomain.getDomain(),
-        			subDomain.getId().longValue(),
-        			"sdmanager",
-        			subDomain.getUserName(),
-        			"", subDomain.getContactsMobile(),"sdmanager", 0, "global");
-
-        	return new SimpleAuthenticationInfo(su,subDomain.getPassword(),getName());
-        }
-        throw new AccountException("用户名登录失败");
+        
+        
 	}
 
 	/**
@@ -71,10 +56,10 @@ public class ShiroDbRealm extends AuthorizingRealm {
 	@Override
 	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
 		ShiroUser shiroUser = (ShiroUser) principals.getPrimaryPrincipal();
-
-		com.intelgoo.qqvisa.po.UserPo user =  accountService.findUserByLoginNameSimple(shiroUser.domain,shiroUser.mobile);
+		PrimitiveUser userInfo = new PrimitiveUser(); // userInfo = userGetService.getBasicBy(token.getUsername(),token.getPassword(),token.getAddition());   
 		SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
-		info.addRole(user.getRole());
+		info.addRole("admin");
+		info.addStringPermission("user:delete");
 		return info;
 	}
 
@@ -84,7 +69,7 @@ public class ShiroDbRealm extends AuthorizingRealm {
 	@PostConstruct
 	public void initCredentialsMatcher() {
 		//该句作用是重写shiro的密码验证，让shiro用我自己的验证
-        setCredentialsMatcher(new org.apache.shiro.authc.credential.CredentialsMatcher(){
+        setCredentialsMatcher(new org.apache.shiro.authc.credential.SimpleCredentialsMatcher(){
             @Override
             public boolean doCredentialsMatch(AuthenticationToken authcToken, AuthenticationInfo info) {
                    UsernamePasswordToken token = (UsernamePasswordToken) authcToken;
@@ -110,80 +95,34 @@ public class ShiroDbRealm extends AuthorizingRealm {
 	 */
 	public static class ShiroUser implements Serializable {
 		private static final long serialVersionUID = -1373760761780840081L;
-		public String domain;
-		public Long id;
-		public String email;
-		public String name;
-		public String mobile;
-		public String role;
-		public Integer emEnterpriseId;
-		private String userType;
-		public String belong;
-		public String dr = "n";
-
-		public boolean useVspRawPrice(){
-
-			if(domain == null)
-				domain = "woigo";
-			if(dr == null)
-				dr = "n";
-			return !domain.equals("woigo") && dr.equals("y");
-		}
-
-		public ShiroUser(String domain,Long id, String userType, String name, String email, String mobile, String role, Integer emEnterpriseId,String belong) {
-			this.domain = domain;
+		private Integer id;
+		private String  username;
+		private Object  addition;
+		
+		public ShiroUser(Integer id, String username, Object addition){
 			this.id = id;
-			this.email = email;
-			this.name = name;
-			this.mobile = mobile;
-			this.role = role;
-			this.emEnterpriseId = emEnterpriseId;
-			this.userType = userType;
-			this.belong = belong;
+			this.username = username;
+			this.addition = addition;
+		}
+		
+		public String getUsername() {
+			return username;
 		}
 
-		public void setUserType(String val){
-			assert("user".equals(val) || "manager".equals(val));
-			this.userType = val;
-		}
-		public boolean isUser(){
-			return "user".equals(this.userType);
-		}
-		public boolean isManager(){
-			return "manager".equals(this.userType);
-		}
-		public boolean isSdManager(){
-        	return "sdmanager".equals(this.userType);
-       	}
-
-		public boolean isAdmin(){
-			return "manager".equals(this.userType) && "admin".equals(this.role.toLowerCase());
+		public void setUsername(String username) {
+			this.username = username;
 		}
 
-		public String getName() {
-			if(this.name==null)
-				return this.mobile;
-			else
-				return this.name;
-		}
-		public String getUserType(){
-			return this.userType;
+		public Object getAddition() {
+			return addition;
 		}
 
-		/**
-		 * 本函数输出将作为默认的<shiro:principal/>输出.
-		 */
-		@Override
-		public String toString() {
-			return name;
+		public void setAddition(Object addition) {
+			this.addition = addition;
 		}
 
-		/**
-		 * 重载hashCode,只计算mobile;
-		 */
-		@Override
-		public int hashCode() {
-			return Objects.hashCode(mobile);
+		public void setId(Integer id) {
+			this.id = id;
 		}
 
 		/**
@@ -201,40 +140,7 @@ public class ShiroDbRealm extends AuthorizingRealm {
 				return false;
 			}
 			ShiroUser other = (ShiroUser) obj;
-			if (mobile == null) {
-				if (other.mobile != null) {
-					return false;
-				}
-			} else if (!mobile.equals(other.mobile)) {
-				return false;
-			}
-			if(!domain.equals(other.domain))
-				return false;
-			return true;
-		}
-
-		public String getRole() {
-			return role;
-		}
-
-		public void setRole(String role) {
-			this.role = role;
-		}
-
-		public Integer getEmEnterpriseId() {
-			return emEnterpriseId;
-		}
-
-		public void setEmEnterpriseId(Integer emEnterpriseId) {
-			this.emEnterpriseId = emEnterpriseId;
-		}
-
-		public Long getId() {
-			return id;
-		}
-
-		public void setId(Long id) {
-			this.id = id;
+			return this.id.equals(other.id);
 		}
 	}
 }
